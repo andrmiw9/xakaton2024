605c357a114c69dccc6b8daa178ef9a57bf342d2
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2024-10-27 02:11:54 +0300

    Восстановил настройку логгера и причесал utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,13 @@
-import numpy as np
+""" Различные вспомогательные функции """
+
+from sys import stdout
+
+import matplotlib.pyplot as plt
 import rasterio
 import torch
-import matplotlib.pyplot as plt
+from colorama import Back
+from loguru import logger
+
 
 def tiff_to_tensor(tif_path):
     """
@@ -19,11 +25,12 @@ def tiff_to_tensor(tif_path):
         # Читаем все каналы изображения и преобразуем в numpy-массив
         img_array = src.read()  # Получаем массив с форматом (channels, height, width)
     img_tensor = torch.from_numpy(img_array).float()
-    img_tensor = torch.clamp(img_tensor, min = 0, max = 5000)
+    img_tensor = torch.clamp(img_tensor, min=0, max=5000)
     img_tensor = img_tensor / 5000
     return img_tensor
 
-def plot_channels(img_tensor, rgb=[2, 1, 0]):
+
+def plot_channels(img_tensor, rgb=None):
     """
     Визуализирует выбранные каналы тензора изображения.
 
@@ -33,6 +40,9 @@ def plot_channels(img_tensor, rgb=[2, 1, 0]):
 
     """
     # Проверяем, что в img_tensor достаточно каналов
+    if rgb is None:
+        rgb = [2, 1, 0]
+
     assert img_tensor.shape[0] > max(rgb), "Выбранные каналы отсутствуют в тензоре"
 
     # Извлекаем три выбранных канала и конвертируем их в numpy
@@ -45,7 +55,30 @@ def plot_channels(img_tensor, rgb=[2, 1, 0]):
         raise ValueError("Должно быть выбрано ровно 3 канала для RGB изображения")
 
     # Отображаем изображение
-    plt.figure(figsize=(20,16))
+    plt.figure(figsize=(20, 16))
     plt.imshow(selected_channels)
     plt.axis('off')
     plt.show()
+
+
+# формат и цвета логов
+log_format: str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>[<level>{level}</level>]" \
+                  "<cyan>[{extra[object_id]}]</cyan>" \
+                  "<magenta>{function}</magenta>:" \
+                  "<cyan>{line}</cyan> - <level>{message}</level>"
+
+
+def logger_set_up():
+    """ Loguru set up """
+
+    logger.remove()  # this removes duplicates in the console if we use the custom log format
+    logger.configure(extra={"object_id": "None"})  # Default values if not bind extra variable
+    logger.level("HL", no=38, color=Back.MAGENTA, icon="🔺")
+    logger.level(f"TRACE", color="<fg #1b7c80>")  # выставить цвет
+    logger.level(f"SUCCESS", color="<bold><fg #2dd644>")  # выставить цвет
+    # for output log in console
+    logger.add(sink=stdout,
+               format=log_format,
+               colorize=True,
+               enqueue=True,  # for better work of async
+               level=1)
