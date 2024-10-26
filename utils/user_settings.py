21d308e0cece6100eb885f49b2052f2f1234474c
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2024-10-27 02:06:54 +0300

    Восстановил константы

--- utils/user_settings.py
+++ utils/user_settings.py
@@ -1,1 +1,4 @@
-geojson_file_path: str = r"S:\xakaton\train_dataset_skoltech_train\train\osm\9.geojson"
+""" Конфигурационные переменные для пользователя """
+
+IMG_FILE: str = r"S:\xakaton\train_dataset_skoltech_train\train\images\9_1.tif"
+GJ_FILE: str = r"S:\xakaton\train_dataset_skoltech_train\train\osm\9.geojson"
